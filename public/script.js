32303dcbd5a77c1677b44094f807a5ab39dbb295
claucanchin <claucanchin@gmail.com>
2019-05-07 01:35:40 +0800

    test populate form with query result and add marker

--- public/script.js
+++ public/script.js
@@ -11,7 +11,9 @@ function initMap() {
 
     map = new google.maps.Map(
         document.getElementById('map'), { center: Singapore, zoom: 11 });
+
     autoComplete = new google.maps.places.Autocomplete(document.querySelector('input'));
+
     autoComplete.bindTo('bounds', map);
 
     autoComplete.addListener('place_changed', handlePlaceChanged);
@@ -19,7 +21,26 @@ function initMap() {
 
 function handlePlaceChanged() {
     infowindow.close();
-    // marker.setVisible(false);
     const place = autoComplete.getPlace();
-    console.log('place', place);
-}
+    // console.log('place', place);
+    const lat = autoComplete.getPlace().geometry.location.lat();
+    const long = autoComplete.getPlace().geometry.location.lng();
+    const myLatLng = new google.maps.LatLng(lat, long);
+
+    document.getElementById("name").value = place.name;
+    document.getElementById("address").value = place.formatted_address;
+    document.getElementById("business_hour").value = place.opening_hours.weekday_text;
+
+    // const hoursArray = place.opening_hours.weekday_text
+    // hoursArray.forEach(function(day) {
+    //     console.log(day);
+    // });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        title: "Hello World!"
+    });
+
+    marker.setMap(map);
+
+};
