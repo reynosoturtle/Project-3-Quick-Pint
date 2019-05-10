240be961a42ddbe8ed7e254a2fff3e0651556e42
seeo <aragonlotr5@gmail.com>
2019-05-10 15:22:10 +0800

    switching over to the gtibook version of the emailer instructions

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,10 @@
 class UserMailer < ApplicationMailer
-
+#This is a mailer. They also have methods called 'actions' and use views to structure the content
   def welcome_email(user)
     @user = user
     mail(to: @user.email, subject: 'Welcome to Quick Pint!')
   end
 
-  def create
-    @user = User.new(user_params)
-      if @user.save
-        UserMailer.welcome_email(@user).deliver_now
-      end
-  end
+
 
 end
