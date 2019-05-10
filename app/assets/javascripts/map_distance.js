2353d7f4d74d30cc51b3a88c655ad012d05f29e0
claudia <claucanchin@gmail.com>
2019-05-10 14:00:45 +0800

    got distance to work and show on index page

--- app/assets/javascripts/map_distance.js
+++ app/assets/javascripts/map_distance.js
@@ -2,26 +2,27 @@ if (window.location.pathname == '/bars' || window.location.pathname == '/') {
 
     navigator.geolocation.getCurrentPosition(position => {
 
-        const myLat = position.coords.latitude
-        const myLong = position.coords.longitude
+        const myLat = position.coords.latitude;
+        const myLong = position.coords.longitude;
 
-        let barCoords = document.querySelectorAll('.latLong')
+        let barCoords = document.querySelectorAll('.latLong');
 
-        for (let i = 0; i< barCoords.length;i++){
+        for (let i = 0; i < barCoords.length; i++) {
 
-            const barDistArr = barCoords[i].innerHTML.split(',')
-            for (let j in barDistArr){
-                barDistArr[j]=parseFloat(barDistArr[j])
+            const barCoordsArr = barCoords[i].innerHTML.split(',');
 
+            for (let j in barCoordsArr) {
+                barCoordsArr[j] = parseFloat(barCoordsArr[j])
+            };
+
+            const distance = Math.sqrt(Math.pow((myLat - barCoordsArr[0]), 2) + Math.pow((myLong - barCoordsArr[1]), 2)) * 110.567;
+
+            function round(value) {
+                return Number(Math.round(value + 'e' + 2) + 'e-' + 2);
             }
-            console.log(barDistArr)
 
-            const distance = Math.sqrt(Math.pow((myLat-barDistArr[0]),2)+Math.pow((myLong-barDistArr[1]),2)) * 110.567
-
-            console.log(distance)
-
-            barCoords[i].innerText = distance
-            barCoords[i].style.visibility = 'visible'
+            barCoords[i].innerText = round(distance) + ' km';
+            barCoords[i].style.visibility = 'visible';
         }
     })
 }
