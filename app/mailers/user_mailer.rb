0afd41e9856899c08b2282b6a1c4ce7620603c20
seeo <aragonlotr5@gmail.com>
2019-05-10 11:43:58 +0800

    reading the documentation for rails for user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,15 @@
 class UserMailer < ApplicationMailer
 
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Welcome to Quick Pint!')
+  end
+
   def create
     @user = User.new(user_params)
       if @user.save
         UserMailer.welcome_email(@user).deliver_now
       end
-  end
-
-  def welcome_email(user)
-    @user = user
-    mail(to: @user.email, subject: 'Welcome to Quick Pint!')
   end
 
 end
